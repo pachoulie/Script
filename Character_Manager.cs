d72224de5d9735f1d06a572665a88372251af686
pachoulie <souy.angelina@gmail.com>
2014-03-20 10:51:48 +0900

    Unity Script
    Character Manager
    Character Motor
    Ne compile pas (Faut du debug)

---
+++ Character_Manager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Manager : MonoBehaviour {
+
+	public static Character_Manager 	Instance;
+	public CharacterController			CharacterControllerComponent;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	void Update()
+	{
+		//Constantly check for a camera
+		//Call ControllerInput()
+		//Call the ControlledUpdate() method of Character_Motor via the Singleton (created later…)
+		Character_Motor.Instance.ControlledUpdate();
+	}
+}
---
+++ Character_Motor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Motor : MonoBehaviour {
+
+	public static	Character_Motor Instance;
+	public Vector3	MoveVector;
+	public float	SpeedLimit;
+
+	void Awake()
+	{
+		//Store an Instance of itself
+		Instance = this;
+	}
+
+	public void ControlledUpdate()
+	{
+		//Call AlignCharacterToCameraDirection()
+		ProcessMotion();
+	}
+
+	public void ProcessMotion()
+	{
+		//Convert Vector to World Space
+		var WorldPosition = transform.TransformDirection(MoveVector);
+
+		float magnitude = WorldPosition.sqrMagnitude;
+
+		//Normalize Vector
+		if (magnitude > 1)
+			WorldPosition.Normalize();
+
+		//Multiply magnifier
+		WorldPosition = WorldPosition * SpeedLimit;
+
+		//Convert to unit/second
+		WorldPosition *= Time.deltaTime;
+
+		//Move character
+
+	}
+
+	public void AlignCharacterToCameraDirection()
+	{
+		//Replace the characters Y rotation with the cameras Y rotation
+	}
+}
