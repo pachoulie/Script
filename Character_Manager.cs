fb3d3feda1a5f45ca3907f2e226b32c5f5b7ca43
poksme <poksme@gmail.com>
2014-03-20 17:41:55 +0900

    Updated motor and manager according to UML

--- Character_Manager.cs
+++ Character_Manager.cs
@@ -1,21 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character_Manager : MonoBehaviour {
+// Class description
+// Process Player input
+// Convert keypresses to a 3D Vector
+// Perform a Constant  Camera Checlk
 
+
+public class Character_Manager : MonoBehaviour {
+	
 	public static Character_Manager 	Instance;
 	public CharacterController			CharacterControllerComponent;
-
+	
+	
 	void Awake()
 	{
 		Instance = this;
 	}
-
+	
 	void Update()
 	{
 		//Constantly check for a camera
 		//Call ControllerInput()
 		//Call the ControlledUpdate() method of Character_Motor via the Singleton (created later…)
 		Character_Motor.Instance.ControlledUpdate();
+	}
+	
+	// Updates the MoveVector in Character_Motor
+	void ControllerInput() {
+		// Calls processMotion();
+		
+	}
+	
+	void ActionInput() {
+		// if jump button pressed
+		// call delegatejump 
+	}
+	
+	void DelegateJump() {
+		// Call Character_Motor jump
 	}
 }
--- Character_Motor.cs
+++ Character_Motor.cs
@@ -1,47 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character_Motor : MonoBehaviour {
+// Class description
+// Convert the 33D Vector to world Space
+// Update the Character Rotation with Camera Rotation
 
+public class Character_Motor : MonoBehaviour {
+	
 	public static	Character_Motor Instance;
 	public Vector3	MoveVector;
 	public float	SpeedLimit;
-
+	
 	void Awake()
 	{
 		//Store an Instance of itself
 		Instance = this;
 	}
-
+	
 	public void ControlledUpdate()
 	{
 		//Call AlignCharacterToCameraDirection()
 		ProcessMotion();
 	}
-
+	
+	
 	public void ProcessMotion()
 	{
+		// Save MoveVector.y and reapply as VerticalVelocity
+		
 		//Convert Vector to World Space
 		var WorldPosition = transform.TransformDirection(MoveVector);
-
+		
 		float magnitude = WorldPosition.sqrMagnitude;
-
+		
 		//Normalize Vector
 		if (magnitude > 1)
 			WorldPosition.Normalize();
-
+		
 		//Multiply magnifier
 		WorldPosition = WorldPosition * SpeedLimit;
-
+		
 		//Convert to unit/second
 		WorldPosition *= Time.deltaTime;
-
+		
 		//Move character
-
+		
 	}
-
+	
 	public void AlignCharacterToCameraDirection()
 	{
 		//Replace the characters Y rotation with the cameras Y rotation
+	}
+	
+	void ApplyGravity() {
+	}
+	
+	void Jump() {
 	}
 }
