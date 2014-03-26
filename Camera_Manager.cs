a7a7e6aef63f7682dc5e1f2e788b26f74e3f7dc8
Bertrand Boustany <bertrand@boustany.me>
2014-03-26 18:52:03 +0900

    Finished, cleaned up working
    Lot of informations here :
    http://webcache.googleusercontent.com/search?q=cache:3b7VL4eYHVsJ:https:
    //www.3dbuzz.com/forum/threads/202405-Slight-Problem-With-The-Camera-In-
    The-Third-Person-Character-System+&cd=2&hl=fr&ct=clnk&gl=fr&lr=lang_en%7
    Clang_fr

--- Camera_Manager.cs
+++ Camera_Manager.cs
@@ -5,68 +5,73 @@ public class Camera_Manager : MonoBehaviour {
 	
 	public static Camera_Manager Instance;
 	public Transform TargetLookAt;
+	
+	public float Dist = 5f;
+	public float MinDist = .25f;
+	public float MaxDist = 3f;
 
-	private float minY = 1;
-	private float maxY = 100;
-	private float minZ = 1;
-	private float maxZ = 100;
-	private Vector3 ValidCamPos;
+	public float MouseSpeed = 5f;
+
+	public float MinY = -40f;
+	public float MaxY = 80f;
+
+	private float mouseX = 0f;
+	private float mouseY = 0f;
+	private float defaultDist = 0f;
+
+	private Vector3 position = Vector3.zero;
 	
-	private Vector2 MouseAxis;
-	
-	void Awake()
-	{
-		//Store an Instance of itself
+	void Awake() {
 		Instance = this;
 	}
-	
-	// Use this for initialization
-	void Start () {
-		// Validate camera position using Mathf.Clamp & Save the validated camera position
-		ValidCamPos = Camera.mainCamera.transform.position;
-		ValidCamPos = new Vector3 (
-			ValidCamPos.x,
-			Mathf.Clamp(ValidCamPos.y, minY, maxY),
-			Mathf.Clamp(ValidCamPos.z, minZ, maxZ)
-		);
-		Camera.mainCamera.transform.LookAt(TargetLookAt);
-		InitialCameraPosition();
+
+	void Start() {
+		// Validate camera position  using Mathf.Clamp
+		Dist = Mathf.Clamp (Dist, MinDist, MaxDist);
+		
+		// Save the validated camera position
+		defaultDist = Dist;
+		
+		// Call InitialCameraPosition()
+		InitialCameraPosition ();
 	}
-	
-	void InitialCameraPosition() {
+
+	void LateUpdate() {
+		// Call VerifyUserMouseInput ()
+		VerifyUserMouseInput ();
+	}
+
+	// Rotates the camera based on the users inputs
+	void VerifyUserMouseInput()
+	{
+		//Check if the right mouse button is depressed (not required but useful for easy debugging)
+		if (!Input.GetMouseButton (1)) {
+			mouseX += Input.GetAxis("Mouse X") * MouseSpeed;
+			mouseY -= Input.GetAxis("Mouse Y") * MouseSpeed;
+		}
+
+		// Clamp/limit mouseY, store the result in the appropriate variable
+		mouseY = ClampAngle (mouseY, MinY, MaxY);
+
+		Vector3 direction = new Vector3 (0, 0, -Dist);
+		Quaternion rotation = Quaternion.Euler (mouseY, mouseX, 0);	
+		position = TargetLookAt.position + rotation * direction;
+		
+		// Update Position
+		transform.position = position;
+		transform.LookAt (TargetLookAt);
+	}
+
+	public void InitialCameraPosition()
+	{
 		// Set the default value for both mouse axis
-		MouseAxis = new Vector2(); // SET TO 0
+		mouseX = 0;
+		mouseY = 10f;
 		
 		// Set the validated initial camera position
-		Camera.mainCamera.transform.position = ValidCamPos;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	void LateUpdate() {
-		VerifyUserMouseInput();
+		Dist = defaultDist;
 	}
 
-	void VerifyUserMouseInput ()
-	{
-		// Check if the right mouse button is depressed (not required but useful for easy debugging)
-		if (!Input.GetMouseButtonDown(1)) { // DOESN T WORK
-			// Rotates the camera based on the users inputs
-				// SET THE TARGET AS PIVOT POINT
-				// ROTATE
-				// LOOK AT THE TARGET
-			
-			// DEBUG
-			Camera.mainCamera.transform.RotateAround(TargetLookAt.position, Vector3.up, -20 * Time.deltaTime); // Positive or negative for left or right
-			Camera.mainCamera.transform.RotateAround(TargetLookAt.position, Vector3.left, 20 * Time.deltaTime); // Positive or negative for left or right			
-		}
-	}
-
-	
-	
 	public static void InitialCameraCheck() {
 		Camera_Manager cameraManager;		
 		GameObject mainCamera;
@@ -93,6 +98,15 @@ public class Camera_Manager : MonoBehaviour {
 		}
 		
 		// Save the target look at value
-		cameraManager.TargetLookAt = targetLookAt.transform; 
+		cameraManager.TargetLookAt = targetLookAt.transform;
 	}
+	
+	private float ClampAngle (float angle, float min, float max) {
+    	if (angle < -360)
+        	angle += 360;
+	    if (angle > 360)
+	        angle -= 360;
+	    return Mathf.Clamp (angle, min, max);
+	}
+
 }
